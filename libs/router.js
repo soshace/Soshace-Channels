6dc294fdf6ff979ac5111aaf79f5411dd17496ba
insanegrv <insanegrv@gmail.com>
2016-02-11 13:19:14 +0300

    Routes for groups

--- libs/router.js
+++ libs/router.js
@@ -2,12 +2,32 @@ Router.configure({
   layoutTemplate: 'main'
 });
 
+Router.route('/', {
+  name: 'flow',
+  template: 'flow'
+});
+
+Router.route('/login', {
+  template: 'login'
+});
+
+Router.route('/register', {
+  template: 'register',
+});
+
+Router.route('/groups', {
+  name: 'groups',
+  template: 'group-list'
+});
+
 Router.route('/about', {
   template: 'about'
 });
 
-Router.route('/register');
-
-Router.route('/login');
-
-Router.route('/group-list');
+Router.route('/group/:_id', {
+  template: 'group',
+  data: function() {
+    var currentGroup = this.params._id;
+    return Groups.findOne({ _id: currentGroup});
+  }
+});
