4ffa6e73bbeb214b70d8a92a3c43e881c8a3d07c
VitaliiZhukov <zhukov-vit@yandex.ru>
2016-02-19 19:31:14 +0300

    Add trello items to channel

--- client/views/channel.js
+++ client/views/channel.js
@@ -1,3 +1,5 @@
+var boards = [];
+
 Template.channel.events({
   'click .channel__delete': function(event) {
     event.preventDefault();
@@ -97,7 +99,88 @@ Template.channel.helpers({
 
     // Return channel members logins
     return Meteor.users.find(selector, options);
+  },
+
+  trelloBoards: function() {
+    return Session.get('trelloBoards');
   }
+});
+
+Template.channel.onRendered(function() {
+  if (!this._rendered) {
+    this._rendered = true;
+    Trello.authorize({
+      type: 'popup',
+      name: 'Trello dashboard',
+      scope: {
+        read: true,
+        write: false
+      },
+      expiration: 'never',
+      success: loadBoards,
+      error: function() {
+        console.log('Failed auth');
+      }
+    });
+  }
+});
+
+
+var loadBoards = function() {
+  console.log('Trello boards loading started');
+  Trello.get(
+    '/members/me/boards/',
+    function(result) {
+      boards = result;
+      for (var i = result.length - 1; i >= 0; i--) {
+        loadLists(i);
+      };
+      console.log(result);
+
+      result[0].lists = [1, 2, 3];
+      Session.set('trelloBoards', result);
+      console.log('Trello boards loading finished');
+    },
+    function() {
+      console.log('Failed to load boards');
+    }
+  );
+};
+
+var loadLists = function(index) {
+  Trello.get(
+    '/boards/' + boards[index].id + '/lists',
+    function(result) {
+      boards[index].lists = result;
+
+      for (var i = result.length - 1; i >= 0; i--) {
+        loadCards(index, i);
+      };
+    },
+    function() {
+      console.log('Failed to load list!');
+    }
+  );
+};
+
+var loadCards = function(boardIndex, listIndex) {
+  console.log(boards[boardIndex].lists[listIndex].id);
+  Trello.get(
+    '/lists/' + boards[boardIndex].lists[listIndex].id + '/cards',
+    function(result) {
+      boards[boardIndex].lists[listIndex].cards = result;
+
+      Session.set('trelloBoards', boards);
+    },
+    function() {
+      console.log('Failed to load card!');
+    }
+  );
+};
+
+Template.registerHelper('formatDate', function(dt) {
+  var date = new Date(dt);
+  return date.getFullYear()+'/'+date.getMonth()+'/'+date.getDay()+' '+date.getHours()+':'+date.getMinutes();
 });
 
 // Template.channel.helpers({
