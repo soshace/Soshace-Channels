58cef537f6429f1a201b505e8856b6c2eb7221af
VitaliiZhukov <zhukov-vit@yandex.ru>
2016-05-27 11:37:51 +0300

    Removed aux script

--- server/auxscript.js
+++ server/auxscript.js
@@ -1,16 +1,15 @@
 Meteor.startup(function(){
-	var users = Meteor.users.find().fetch();
-	_.map(users, function(user){
-		if (!user.profile.serviceTokens && user.profile.services){
-			Meteor.users.update({_id: user._id}, {
-				$set: {
-					'profile.serviceTokens': [{
-						serviceName: 'github',
-						token: user.profile.services.token
-					}]
-				}
-			});
-			console.log('user tokens updated');
-		}
-	})
+	// var users = Meteor.users.find().fetch();
+	// _.map(users, function(user){
+	// 	var tokens = user.profile.serviceTokens;
+	// 	if (!_.findWhere(tokens, {serviceName: 'github'}).token)
+	// 	if (!user.profile.serviceTokens && user.profile.services){
+	// 		Meteor.users.update({_id: user._id}, {
+	// 			$set: {
+	// 				'profile.serviceTokens': []
+	// 			}
+	// 		});
+	// 		console.log('user tokens updated');
+	// 	}
+	// })
 });
