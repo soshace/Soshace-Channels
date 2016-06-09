fa7f64afe650df30e5b28d2c8f6134ccc02b58c1
VitaliiZhukov <zhukov-vit@yandex.ru>
2016-06-09 15:44:03 +0300

    Yandex channel in progress

--- server/services.js
+++ server/services.js
@@ -114,29 +114,26 @@ Meteor.methods({
 		console.log(url);
 		return Meteor.http.get(url, options, function(error, result) {
 			var Imap = Npm.require('imap');
-			var xoauth2 = Npm.require('xoauth2');
+			// var xoauth2 = Npm.require('xoauth2');
 
-			xoauth2gen = xoauth2.createXOAuth2Generator({
-				user: result.data.login,
-				clientId: Meteor.settings.public['yandex_client_id'],
-				clientSecret: Meteor.settings.private['yandex_client_secret'],
-				accessToken: token
-			});
+			// xoauth2gen = xoauth2.createXOAuth2Generator({
+			// 	user: result.data.login,
+			// 	clientId: Meteor.settings.public['yandex_client_id'],
+			// 	clientSecret: Meteor.settings.private['yandex_client_secret'],
+			// 	accessToken: token
+			// });
 
-			xoauth2gen.getToken(function(err, token) {
-				if (err) {
-					return console.log(123);
-				}
-				// console.log("AUTH XOAUTH2 " + token);
-			});
+			// xoauth2gen.getToken(function(err, token) {
+			// 	if (err) {
+			// 		return console.log(123);
+			// 	}
+			// });
 
-			// user=<логин>\@yandex.ru\001auth=Bearer <OAuth-токен>\001\001
-			var s = 'user=zhukov_vi\@soshace.com\001auth=Bearer '+token+'\001\001';
+			var s = 'user=superstringt@yandex.ru\001auth=Bearer ' + token + '\001\001';
 			var t = new Buffer(s).toString('base64');
-			console.log(s);
 			var connParams = {
 				id: 13,
-				xoauth2: 'A01AUTHENTICATEXOAUTH2'+t,
+				xoauth2: t,
 				host: 'imap.yandex.com',
 				port: 993,
 				tls: 1,
@@ -148,14 +145,83 @@ Meteor.methods({
 			};
 
 			imap = new Imap(connParams);
-			console.log(123);
-			imap.openBox('INBOX', true, function(result, error){
-				console.log(error);
-				console.log(result);
+			imap.once('ready', function() {
+				imap.openBox('INBOX', true, function(err, box) {
+					// if (err) throw err;
+					// var f = imap.seq.fetch('1:3', {
+					// 	bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+					// 	struct: true
+					// });
+					// f.on('message', function(msg, seqno) {
+					// 	console.log('Here Message #%d', seqno);
+					// 	var prefix = '(#' + seqno + ') ';
+					// 	msg.on('body', function(stream, info) {
+					// 		var buffer = '';
+					// 		stream.on('data', function(chunk) {
+					// 			buffer += chunk.toString('utf8');
+					// 		});
+					// 		stream.once('end', function() {
+					// 			console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+					// 		});
+					// 	});
+					// 	msg.once('attributes', function(attrs) {
+					// 		console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+					// 	});
+					// 	msg.once('end', function() {
+					// 		console.log(prefix + 'Finished');
+					// 	});
+					// });
+					// f.once('error', function(err) {
+					// 	console.log('Fetch error: ' + err);
+					// });
+					// f.once('end', function() {
+					// 	console.log('Done fetching all messages!');
+					// 	imap.end();
+					// });
 
+					var f = imap.seq.fetch('1:3', {
+						bodies: ['HEADER','TEXT']
+					});
+					f.on('message', function(msg, seqno) {
+						console.log('Message #%d', seqno);
+						var prefix = '(#' + seqno + ') ';
+						msg.on('body', function(stream, info) {
+							// if (info.which === 'TEXT')
+								// console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
+							var buffer = '',
+								count = 0;
+							stream.on('data', function(chunk) {
+								count += chunk.length;
+								buffer += chunk.toString('utf8');
+								// console.log('BUFFER', buffer) //HEre i am able to view the body
+								// if (info.which === 'TEXT')
+									// console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
+							});
+							stream.once('end', function() {
+								console.log(Imap.parseHeader(buffer));
+								// if (info.which !== 'TEXT')
+									// console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+								// else
+									// console.log(prefix + 'Body [%s] Finished', inspect(info.which));
+							});
+						});
+						msg.once('attributes', function(attrs) {
+							// console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+						});
+						msg.once('end', function() {
+							console.log(prefix + 'Finished');
+						});
+					});
+					f.once('error', function(err) {
+						console.log('Fetch error: ' + err);
+					});
+					f.once('end', function() {
+						console.log('Done fetching all messages!');
+						imap.end();
+					});
+				});
 			});
-
-
+			imap.connect();
 		});
 	}
 });
