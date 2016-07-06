f772f01c86229f67b8525c44b08d1b2c121b7f0f
VitaliiZhukov <zhukov-vit@yandex.ru>
2016-07-06 19:02:55 +0300

    Removed code for contacts, associated emails and invites from channel process

--- client/views/channels/channel.js
+++ client/views/channels/channel.js
@@ -1,10 +1,7 @@
 var deps = new Deps.Dependency(),
-  // associatedEmails = [],
   blocks, // Array of loaded blocks (commits, boards etc)
   plugin,
   channelData,
-  // channelMembers, // List of this channel members
-  // contacts, // Users contacts to determine who is not in channel yet
   loading = true;
 
 Template.channel.events({
@@ -28,8 +25,6 @@ Template.channel.events({
     event.preventDefault();
     var parameter = $('.channel__member-input').val();
 
-    // var select = document.querySelector('[name=user-to-channel]');
-    // var userId = select.options[select.selectedIndex].value;
     Meteor.call('addMemberByParam', channelData._id, parameter, function(error, results) {
       if (error) {
         if (error.reason === ERRORS.userNotFound) {
@@ -51,52 +46,6 @@ Template.channel.events({
     });
   },
 
-  // 'click .channel__invite-unregistered': function(event, template) {
-  //   var emailForInvite = this.email,
-  //     channelCreatorId = Meteor.userId(),
-  //     invitation = {
-  //       email: emailForInvite,
-  //       channelId: channelData._id,
-  //       channelCreatorId: channelCreatorId
-  //     };
-
-  //   Meteor.call('sendInvitation', invitation, function(error, response) {
-  //     if (error) {
-  //       Bert.alert(error.reason, 'warning');
-  //     } else {
-  //       if (response === 'Invite already exist.') {
-  //         Bert.alert('Invite to this email is already exist. You can revoke it and try again.', 'info');
-  //       } else {
-  //         // TODO: remove email from associated email list?
-  //         Bert.alert('Invitation send to ' + emailForInvite + '.', 'success');
-  //       }
-  //     }
-  //   });
-  // },
-
-  // 'click .channel__invite-registered': function(event, template) {
-  //   var userId = event.target.id,
-  //     selector = {
-  //       _id: userId
-  //     },
-  //     options = {
-  //       fields: {
-  //         username: 1,
-  //       }
-  //     };
-
-  //   userToAdd = Meteor.users.findOne(selector, options);
-
-  //   // Request from inviter to user
-  //   Meteor.call('requestContact', userToAdd.username);
-  //   // Add user to channel
-  //   Meteor.call('addMember', channelData._id, userId);
-  // },
-
-  // 'click .channel__add-contact-to-channel': function(event, template) {
-  //   Meteor.call('addMember', channelData._id, event.target.id);
-  // },
-
   'click .previous-link': function(event, template) {
     loading = true;
     deps.changed();
@@ -113,22 +62,6 @@ Template.channel.events({
 });
 
 Template.channel.helpers({
-  // contacts: function() {
-  //   var allContacts = Meteor.user().contacts;
-  //   var acceptedUsers = _.where(allContacts, {
-  //     contactStatus: 'accepted'
-  //   });
-
-  //   var selector = {
-  //     _id: {
-  //       $in: _.pluck(acceptedUsers, 'contactId')
-  //     }
-  //   };
-
-  //   contacts = Meteor.users.find(selector).fetch();
-  //   return contacts;
-  // },
-
   members: function() {
     var memberIds = Channels.findOne(channelData._id).members || [];
     Meteor.subscribe('publicUserData', memberIds);
@@ -151,11 +84,6 @@ Template.channel.helpers({
     loading = true;
     return pagination;
   },
-
-  // emails: function() {
-  //   deps.depend();
-  //   return associatedEmails;
-  // },
 
   previewTemplate: function() {
     deps.depend();
@@ -207,5 +135,3 @@ function getBlocksCallback(data, resourceId) {
   loading = false;
   deps.changed();
 };
-
-// function getEmailsCallback(data) {};
