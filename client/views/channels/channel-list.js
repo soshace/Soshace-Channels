e50b5b6790c953e7f6ec5aa7f3d5554f0844f011
VitaliiZhukov <zhukov-vit@yandex.ru>
2016-07-06 13:21:33 +0300

    Changes for channels preview displaying

--- client/views/channels/channel-list.js
+++ client/views/channels/channel-list.js
@@ -1,28 +1,36 @@
 Template.channelList.helpers({
 	channels: function() {
-		var currentUser = Meteor.userId();
-
-		return Channels.find({
-			createdBy: currentUser
+		var channels = Channels.find({
+			createdBy: Meteor.userId()
 		}, {
 			sort: {
 				createdAt: -1
 			}
+		}).fetch();
+
+		_.each(channels, function(item) {
+			item.icon = SERVICES[item.serviceType].icon;
 		});
+
+		return channels;
 	},
 
 	guestchannels: function() {
-		var currentUser = Meteor.userId();
-
-		return Channels.find({
-			members: currentUser
+		var channels = Channels.find({
+			members: Meteor.userId()
 		}, {
 			sort: {
 				createdAt: -1
 			}
-		});
-	},
+		}).fetch();
 
+		_.each(channels, function(item) {
+			item.icon = SERVICES[item.serviceType].icon;
+		});
+
+		return channels;
+	},
+	
 	channelHost: function(userId) {
 		return Meteor.users.findOne(userId).username;
 	}
