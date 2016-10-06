d66ff1c440dac72de49a418cc95b5bc5f92fc1e8
insanegrv <insanegrv@gmail.com>
2016-10-06 19:59:26 +0300

    Methods for mailboxes db

---
+++ server/controllers/mailboxes.js
@@ -0,0 +1,31 @@
+// Methods working with 'Mailboxes' collection
+Meteor.methods({
+  'saveDialogsToDatabase': function(channelId, mailData) {
+    var currentUserId = this.userId,
+        newMailData = {
+          userId: currentUserId,
+          channelId: channelId,
+          createdAt: new Date(),
+          dialogs: mailData
+        };
+
+    if (!currentUserId) {
+      throw new Meteor.Error('not-logged-in', 'You are not logged-in.');
+    }
+
+    return Mailboxes.insert(newMailData);
+  },
+
+  'checkMailboxesDB': function(channelId) {
+    var currentUserId = this.userId;
+
+    if (!currentUserId) {
+      throw new Meteor.Error('not-logged-in', 'You are not logged-in.');
+    }
+
+    return Mailboxes.findOne({
+      'channelId': channelId,
+      'userId': currentUserId
+    })
+  }
+})
