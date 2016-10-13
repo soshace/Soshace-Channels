f40ed8884cf009dd7c9425b5c1bed4997c754178
insanegrv <insanegrv@gmail.com>
2016-10-13 19:13:22 +0300

    Create other schema for Mails collection

---
+++ collections/mails.js
@@ -0,0 +1,53 @@
+// 'Mails' collections and it's schema
+
+Mails = new Mongo.Collection('mails');
+
+var Schemas = {};
+
+Schemas.Message = new SimpleSchema({
+  messageId: {
+    type: String
+  },
+  subject: {
+    type: String
+  },
+  body: {
+    type: String
+  },
+  from: {
+    type: String
+  },
+  to: {
+    type: String
+  },
+  additionalInfo: {
+    type: Object,
+    blackbox: true
+  }
+});
+
+Schemas.Folder = new SimpleSchema({
+  name: {
+    type: String
+  },
+  messages: {
+    type: [Schemas.Message]
+  }
+});
+
+Schemas.Mail = new SimpleSchema({
+  userId: {
+    type: String
+  },
+  channelId: {
+    type: String
+  },
+  createdAt: {
+    type: Date
+  },
+  folders: {
+    type: [Schemas.Folder]
+  }
+});
+
+Mails.attachSchema(Schemas.Mail);
